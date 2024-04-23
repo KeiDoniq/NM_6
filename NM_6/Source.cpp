e254ab27b79a078bc67171dc3a8da03068aa9f9c
Kira <kira.doniy@gmail.com>
2024-04-23 19:39:23 +0300

    to be execute and test

--- NM_6/Source.cpp
+++ NM_6/Source.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
 #include "diffSweep.h"
 
+double p(double x) {
+	return 2 * x;
+}
+double f(double x) {
+	return 0;
+}
+double q(double x) {
+	return 10 * x * x;
+}
 
 int main() {
-
+	diffSweep testDiff("data.txt", "output.txt", p, q, f);
 	std::cin.get();
 }
